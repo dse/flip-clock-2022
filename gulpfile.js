1321d278195720c835193600d4890414f94dc2e7
Darren Embry <dse@webonastick.com>
2023-01-02 16:58:50 -0500

    start with a gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,47 @@
 'use strict';
 
 const gulp = require('gulp');
-const $sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
-const $webpack = require('gulp-webpack');
-const $postcss = require('gulp-postcss');
+const webpack = require('webpack-stream');
+const postcss = require('gulp-postcss');
 const browserSync = require('browser-sync');
+const compiler = require('webpack');
 
 function sassTask() {
     return gulp.src(['src/scss/main.scss'], { base: 'src/scss', sourcemaps: true })
-        .pipe($sass())
-        .pipe($postcss([autoprefixer()]))
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
         .pipe(gulp.dest('www/css', { sourcemaps: '.' }));
 }
 
+function webpackTask() {
+    return gulp.src(['src/js/main.js'], { base: 'src/js', sourcemaps: true })
+        .pipe(webpack(require('./webpack.config.js'),
+                       compiler))
+        .pipe(gulp.dest('www/js', { sourcemaps: '.' }));
+}
+
+function watchSassTask(){
+    return gulp.watch(['src/scss/**/*.scss'], {
+        ignoreInitial: false,
+    }, sassTask);
+}
+
+function watchWebpackTask() {
+    return gulp.watch(['src/js/**/*.js'], {
+        ignoreInitial: false,
+    }, webpackTask);
+}
+
+function serveTask() {
+}
+
+const watchTask = gulp.parallel(watchSassTask, watchWebpackTask);
+
 module.exports = {
     sass: sassTask,
+    webpack: webpackTask,
+    watch: watchTask,
+    serve: serveTask,
 };
