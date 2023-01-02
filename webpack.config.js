6832e5c172a572dc3483f46620b520e2096e4d12
Darren Embry <dse@webonastick.com>
2023-01-02 17:11:26 -0500

    babel/webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 'use strict';
+// https://createapp.dev/webpack/no-library
 
 const path = require('path');
 
-module.exports = {
+const config = {
     entry: './src/js/main.js',
     output: {
         path: path.resolve(__dirname + 'www/js'),
         filename: 'main.bundle.js',
     },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devtool: 'source-map',
 };
+
+module.exports = config;
