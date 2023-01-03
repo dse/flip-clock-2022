274c91b3dc4811ae0940a4985733081aedbd5b9e
Darren Embry <dse@webonastick.com>
2023-01-03 01:04:16 -0500

    updated generated css and js

--- www/js/main.bundle.js
+++ www/js/main.bundle.js
@@ -1377,11 +1377,11 @@ function init() {
   if (/(?:^|[?&])arial-?black(?:$|[?&=])/i.test(location.search)) {
     useFontClass('font--arial-black');
   }
-  if (/(?:^|[?&])poppins(?:$|[?&=])/i.test(location.search)) {
-    useFontClass('font--poppins');
+  if (/(?:^|[?&])lato(?:$|[?&=])/i.test(location.search)) {
+    useFontClass('font--lato');
   }
   if (/(?:^|[?&])poppins(?:$|[?&=])/i.test(location.search)) {
-    useFontClass('font--lato');
+    useFontClass('font--poppins');
   }
   if (/(?:^|[?&])times(?:$|[?&=])/i.test(location.search)) {
     useFontClass('font--times');
