6933bac86974f1df17f748126dcddc670f502d74
Darren Embry <dse@webonastick.com>
2023-01-24 15:39:42 -0500

    update css and js bundles

--- www/js/cordova.bundle.js
+++ www/js/cordova.bundle.js
@@ -643,6 +643,9 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
@@ -758,6 +761,30 @@ var SplitFlap = /*#__PURE__*/function () {
     this.flapB = document.createElement('span');
     this.flapC = document.createElement('span');
     this.flapD = document.createElement('span');
+    this.otherFlaps = [];
+    this.otherFlaps.push(document.createElement('span'));
+    this.otherFlaps.push(document.createElement('span'));
+    this.otherFlaps.push(document.createElement('span'));
+    this.otherFlaps.push(document.createElement('span'));
+    var i = 0;
+    for (i = 0; i < this.otherFlaps.length / 2; i += 1) {
+      this.otherFlaps[i].classList.add('clock__flap', 'clock__top-flap', 'clock__flap--other-flap', 'clock__flap--other-flap--top', 'clock__flap--other-flap--top--' + String(i + 1));
+    }
+    for (var j = i; i < this.otherFlaps.length; i += 1) {
+      this.otherFlaps[i].classList.add('clock__flap', 'clock__bottom-flap', 'clock__flap--other-flap', 'clock__flap--other-flap--bottom', 'clock__flap--other-flap--bottom--' + String(i - j + 1));
+    }
+    var _iterator = _createForOfIteratorHelper(this.otherFlaps),
+      _step;
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var otherFlap = _step.value;
+        this.element.appendChild(otherFlap);
+      }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
     this.flapA.classList.add('clock__flap', 'clock__top-flap');
     this.flapB.classList.add('clock__flap', 'clock__bottom-flap');
     this.flapC.classList.add('clock__flap', 'clock__top-flap', 'clock__top-flap--temp');
